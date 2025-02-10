d328f83c1e5822915913feadcae7658ad4576a85
TheSkylerDev <kesch.inno14@gmail.com>
2025-02-10 14:26:54 +0100

    Further preparation for further development work

--- v0.1.0-alpha/stm-main.dart
+++ v0.1.0-alpha/stm-main.dart
@@ -1,5 +1,8 @@
 //STM: Storage Task MAnager (c) 2025 TheSkyler-Dev, developed under MIT license
 
-void main(){
+import 'dart:convert'; // For JSON parsing
+import 'package:flutter/material.dart'; // For Flutter GUI
+
+void main() {
   var banana = 'POTASSIUM'; //This is just a placeholder lmao
 }
